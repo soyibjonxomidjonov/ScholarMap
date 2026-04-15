ae4d3f4c6c1591c64d601c391e7b41ea8e39ef75
Soyibjon <soyibjon12ss@gmail.com>
2026-04-15 22:20:09 +0500

    host update

--- config/settings.py
+++ config/settings.py
@@ -232,5 +232,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "https://scholarmap.uz",
     "https://www.scholarmap.uz",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
