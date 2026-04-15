48fa800bf95a1e71079b7fb4f06c6213d18562e3
Soyibjon <soyibjon12ss@gmail.com>
2026-04-16 00:49:14 +0500

    password qo'shildi

--- api/serializers/user_serializer.py
+++ api/serializers/user_serializer.py
@@ -6,4 +6,4 @@ User = get_user_model()
 class UserSerializerConfig(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "phone_number", "last_name", "sharif","image", "lang", "created_at"]
+        fields = ["id", "username", "password" ,"email", "first_name", "phone_number", "last_name", "sharif","image", "lang", "created_at"]
