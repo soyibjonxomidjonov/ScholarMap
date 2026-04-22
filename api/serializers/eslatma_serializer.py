fd5578e8116a88e95d145385b221f1a463fa96c3
Soyibjon <soyibjon12ss@gmail.com>
2026-04-23 04:13:45 +0500

    filter user

--- api/serializers/eslatma_serializer.py
+++ api/serializers/eslatma_serializer.py
@@ -4,5 +4,6 @@ from api.models import Eslatma
 class EslatmaSerializerConfig(serializers.ModelSerializer):
     class Meta:
         model = Eslatma
-        fields = ["id", "eslatma_matni", "universitet", "user", "eslatma_kun","ogohlantirish_sms" ,"qolgan_kun", "tugash_kun",
+        fields = ["id", "eslatma_matni", "universitet", "user", "eslatma_kun","ogohlantirish_sms" ,"qolgan_kun",
+                  "tugash_kun", 'chat_id',
                   "updated_at", "created_at"]
