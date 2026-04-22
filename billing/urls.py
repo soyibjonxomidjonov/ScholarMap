0cbe77f3ca863b8d15e7dff86deb5508861c78fe
Soyibjon <soyibjon12ss@gmail.com>
2026-04-22 15:55:04 +0500

    click qo'shildi

--- billing/urls.py
+++ billing/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from billing.views import ClickPaymentView, ClickCompleteView, ClickCreatePaymentView
 
 urlpatterns = [
-    path('click/', ClickPaymentView.as_view(), name='click-payment'),
+    path('click/pay/', ClickCreatePaymentView.as_view(), name='click-pay'),          # havola yaratish
+    path('click/prepare/', ClickPaymentView.as_view(), name='click-prepare'),        # Click webhook 1
     path('click/complete/', ClickCompleteView.as_view(), name='click-complete'),
-    path('click/pay/', ClickCreatePaymentView.as_view(), name='click-pay'),
 ]
