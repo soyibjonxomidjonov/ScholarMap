12b26f9d2203857572b1aaee05726d5d602f0c5f
Soyibjon <soyibjon12ss@gmail.com>
2026-04-22 17:05:56 +0500

    gunicorn timeout bug

--- billing/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from billing.views import ClickPaymentView, ClickCompleteView, ClickCreatePaymentView
-
-urlpatterns = [
-    path('click/pay/', ClickCreatePaymentView.as_view(), name='click-pay'),          # havola yaratish
-    path('click/prepare/', ClickPaymentView.as_view(), name='click-prepare'),        # Click webhook 1
-    path('click/complete/', ClickCompleteView.as_view(), name='click-complete'),
-]
